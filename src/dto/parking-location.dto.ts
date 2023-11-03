d9b49a0bc22a35ba7b8508cdc54790ba2449a3d0
“Chin-TT” <20010757@st.phenikaa-uni.edu.vn>
2023-11-03 13:21:07 +0700

    add parking-location dto

---
+++ src/dto/parking-location.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ParkingLocationDTO {
+  @ApiProperty()
+  listblocked?: number[];
+
+  @ApiProperty()
+  listRoad?: number[];
+
+  @ApiProperty()
+  listEmpty?: number[];
+
+  @ApiProperty()
+  floorId?: number;
+}
