e1fa5d91cc34370aa8d6634aac5687fce6383d8c
“Chin-TT” <20010757@st.phenikaa-uni.edu.vn>
2023-11-03 23:54:14 +0700

    fix entity

--- src/entities/monthly_ticket_request.entity.ts
+++ src/entities/monthly_ticket_request.entity.ts
@@ -1,5 +1,6 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
 
+@Entity('MonthlyTicketRequest')
 export class MonthlyTicketRequest {
   @PrimaryGeneratedColumn('increment')
   id: number;
